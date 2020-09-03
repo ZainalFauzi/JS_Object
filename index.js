d9349ded7c609a2986864ebf7e3df5c7fab6c909
ZainalFauzi <fourth.breast@gmail.com>
2020-09-03 17:12:24 +0700

    javascript object

---
+++ index.js
@@ -0,0 +1,65 @@
+let buah = [
+    {
+        name : "Kelapa (Coconut)",
+        color : "Green",
+        latinName : "Cocos nucifera",
+        seedType : "Dicot" 
+    },
+    {
+        name : "Pisang (Banana)",
+        color : "Yellow, Sometimes Green",
+        latinName : "Musa acuminata",
+        seedType : "Monocot"
+    },
+    {
+        name : "Kiwi",
+        color : "Green",
+        latinName : "Actinidia deliciosa",
+        seedType : "Dicot" 
+    },
+    {
+        name : "Manggis (Mangosteen)",
+        color : "Purple",
+        latinName : "Garcinia mangostana",
+        seedType : "Dicot"
+    },   
+    {
+        name : "Nanas (Pineapple)",
+        color : "Yellow",
+        latinName : "Ananas comosus",
+        seedType : "Monocot" 
+    },
+    {
+        name : "Pepaya (Papaya)",
+        color : "Orange, Sometimes Red",
+        latinName : "Carica papaya",
+        seedType : "Dicot"
+    },
+    {
+        name : "Petai (Bitter bean)",
+        color : "Green",
+        latinName : "Parkia speciosa",
+        seedType : "Dicot" 
+    },
+    {
+        name : "Buah naga (Pitaya)",
+        color : "Red & Purple",
+        latinName : "Hylocereus undatus",
+        seedType : "Dicot"
+    },
+    {
+        name : "Delima (Pomegranate)",
+        color : "Red",
+        latinName : "Punica granatum",
+        seedType : "Dicot" 
+    },
+    {
+        name : "Rambutan",
+        color : "Red",
+        latinName : "Nephelium lappaceum",
+        seedType : "Dicot"
+    },
+]
+buah.forEach((listBuah) => {
+    console.log(listBuah)
+})
